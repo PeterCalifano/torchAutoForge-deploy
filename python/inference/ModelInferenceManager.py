5333803a6704c8044eb5c0471342b2c7fbfa9aad
PeterC <petercalifano.gs@gmail.com>
2025-02-19 21:57:09 +0100

    Add placeholder files and todo

---
+++ python/inference/ModelInferenceManager.py
@@ -0,0 +1,8 @@
+from torch import nn
+# TODO this module is intended to facilitate usage of available categories of models, e.g. feature matching, as well as compilation and inference time optimizations. 
+
+
+class ModelInferenceManager:
+    def __init__(self):
+
+        pass
