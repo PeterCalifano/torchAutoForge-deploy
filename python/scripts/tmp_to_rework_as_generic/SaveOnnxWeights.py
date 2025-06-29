a32fe292c4ced06470dfcde72a55ccd197fc2b2a
PeterC <petercalifano.gs@gmail.com>
2025-06-29 13:00:42 +0200

    Initialize library setup

--- python/scripts/tmp_to_rework_as_generic/SaveOnnxWeights.py
+++ python/scripts/tmp_to_rework_as_generic/SaveOnnxWeights.py
@@ -5,6 +5,7 @@ import scipy.io as sio
 import os
 import pathlib
 import argparse as argp
+from autoforgeDeployPy.utils.matlab_utils import Sanitize_matlab_name
 
 # User-options
 onnx_model_path = "onnx_models/ambitious_calf_40_epoch_2213.onnx"
@@ -14,38 +15,7 @@ output_file = "test_data/onnx_weights.mat"
 
 # %% Auxiliary functions for export
 # TODO Move to utils/matlab_utils.py
-def Sanitize_matlab_name(name):
-    """
-    Convert ONNX weight name to MATLAB-compatible field name (max 31 chars)
-    """
-    # Remove onnx:: prefix
-    if name.startswith('onnx::'):
-        name = name[6:]
-    
-    # Replace problematic characters with underscores
-    sanitized = name.replace('.', '_').replace('-', '_').replace('/', '_').replace('\\', '_').replace(':', '_')
-    
-    # Ensure it starts with a letter (MATLAB requirement)
-    if not sanitized[0].isalpha():
-        sanitized = 'w_' + sanitized
-    
-    # Truncate to 31 characters (MATLAB limitation)
-    if len(sanitized) > 31:
-        # Try to keep the end part which is usually more informative (like 'weight', 'bias')
-        if '_' in sanitized:
-            parts = sanitized.split('_')
-            # Keep the last part and truncate the beginning
-            last_part = parts[-1]
-            remaining_chars = 31 - len(last_part) - 1  # -1 for underscore
-            if remaining_chars > 0:
-                truncated_start = sanitized[:remaining_chars]
-                sanitized = truncated_start + '_' + last_part
-            else:
-                sanitized = sanitized[:31]
-        else:
-            sanitized = sanitized[:31]
-    
-    return sanitized
+
 
 # TODO (UM) Move to utils/onnx_utils.py
 # TODO extend to same to json as well (can be useful)
